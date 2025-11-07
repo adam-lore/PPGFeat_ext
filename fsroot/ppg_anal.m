f0b7c8247e7725f7d4fc41e5661dde3b4dbfe1b2
adam-lore <adam.lore@hotmail.se>
2025-11-07 09:44:28 +0100

    Removed superflous zerocrossing function

--- fsroot/ppg_anal.m
+++ fsroot/ppg_anal.m
@@ -677,20 +677,6 @@ methods (Access = private)
         B = B';
         obj.Ssqi = [A B];
     end
-
-    %%created zero crossing function on 9th jan
-    function loc = zerocrossingd(obj,x)
-        inew = 1;
-        r = x;
-        loc =0;
-        for ch = 2:length(r)
-            
-            if (((r(ch-1)< 0) && (r(ch)> 0)) || ((r(ch-1)> 0) && (r(ch)< 0)))
-                loc(inew) = ch;
-                inew = inew + 1;
-            end
-        end
-    end
 end
 
 
